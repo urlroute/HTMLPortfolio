cce830264c8b601ba8d7650ebf0ac2b7843d7b4a
urlroute <urlroute@gmail.com>
2022-01-16 11:57:17 -0800

    pages now snap into center

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,4 +17,4 @@
 
 
 // Option 3 - Smooth Scroll - https: //github.com/cferdinandi/smooth-scroll
-    var scroll = new SmoothScroll('.navbar a[href*="#"]', {speed: 500});
+    // var scroll = new SmoothScroll('.navbar a[href*="#"]', {speed: 500});
